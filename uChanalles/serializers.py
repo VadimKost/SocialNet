813e607e8cc2023fa7f22c6876e13b8fbeff603b
VadimKost <39429087+VadimKost@users.noreply.github.com>
2020-03-22 14:03:25 +0200

    api update

--- uChanalles/serializers.py
+++ uChanalles/serializers.py
@@ -8,11 +8,16 @@ from rest_framework.permissions import IsAuthenticated
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'username', 'email', 'is_staff','id']
 
 # ViewSets define the view behavior.
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
-class UserViewSet(viewsets.ModelViewSet):
+class CurrentUserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    def get_object(self):
+        return self.request.user
+
+    def list(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
--- uChanalles/urls.py
+++ uChanalles/urls.py
@@ -21,7 +21,7 @@ from rest_framework import routers
 from uChanalles.serializers import *
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'current_user', CurrentUserViewSet)
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
